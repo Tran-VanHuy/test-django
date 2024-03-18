c42b66157f90e2c5a4180caa6095e5de6850cd37
TRAN VAN HUY <huy1821051049@gmail.com>
2024-03-18 22:31:11 +0700

    main

--- ui/admin.py
+++ ui/admin.py
@@ -6,10 +6,21 @@ class BlogModelAdmin(admin.ModelAdmin):
 
 	readonly_fields = ['created_at']
 
-	def get_readonly_fields(self, request, obj=None):
-		if obj and obj.user != request.user:
-			return [field.name for field in obj._meta.fields]
-		return self.readonly_fields
+	exclude = ['user']
+	def has_change_permission(self, request, obj=None):
+		if obj is not None and obj.user != request.user:
+			return False
+		return True
+
+	def has_delete_permission(self, request, obj=None):
+		if obj is not None and obj.user != request.user:
+			return False
+		return True
+
+	def save_model(self, request, obj, form, change):
+		if not obj.pk:
+			obj.user = request.user 
+		super().save_model(request, obj, form, change)
 
 # Register your models here.
 admin.site.register(Blog, BlogModelAdmin)
--- ui/views.py
+++ ui/views.py
@@ -1,33 +1,37 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import *
 
 # Create your views here.
 
-class HomePage(TemplateView):
+class HomePage(ListView):
 
+	model = Blog
 	template_name = "home/home.html"
+	context_object_name = "blog"
 
-	def  get_context_data(self, **kwargs):
-		context = super().get_context_data(**kwargs)
+	def  get_queryset(self):
 
-		blog = Blog.objects.all()
-		context["blog"] = blog
+		blog = Blog.objects.all().order_by('-created_at')
+		return blog;
 
-		return context;
+class BlogDetail(DetailView):
 
-class BlogDetail(TemplateView):
-
+	model = Blog
 	template_name = "blog/blog-detail.html"
+	context_object_name = "blogDetail"
 
-	def  get_context_data(self, **kwargs):
+	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
-		slug = self.kwargs['slug']
+		slug = self.kwargs.get('slug')
+		context['blogRelated'] = Blog.objects.exclude(id=slug)[:3]
+		return context
 
-		blogDetail = Blog.objects.filter(id=slug).first()
-		blogRelated = Blog.objects.exclude(id=slug)[0:3]
+	def get_object(self, **kwargs):
+		slug = self.kwargs.get('slug')
+		blog = Blog.objects.filter(id=slug).first()
+		return blog
 
-		context["blogDetail"] = blogDetail
-		context["blogRelated"] = blogRelated
-		return context;
+
+	
 
